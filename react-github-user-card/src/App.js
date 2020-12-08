f4f1b47fb9af6f71d663c998aac1b7625aa2f48f
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-12-08 16:03:37 -0700

    title input and button created to make card with api call

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Jordan"
+    }
+  };
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>React GitHub User Card {this.state.name}</h1>
+        </div>
+        <div>
+          <input placeholder="Search GitHub User"></input>
+          <button>Search</button>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
