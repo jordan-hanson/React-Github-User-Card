47db4cf19587057d5583b986da64500fc7778622
Jordan Hanson <JORDANMERCEDES22@GMAIL.COM>
2020-12-09 16:27:44 -0700

    finished with stretch

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -1,26 +1,80 @@
 import './App.css';
 import React from 'react'
+// import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      name: "Jordan"
-    }
+
+  state = {
+    githubUsers: [],
+    githubUserName: "",
+  }
+
+
+  componentDidMount() {
+    console.log("component mount and axio call coming")
+    setTimeout(() => {
+      axios.get("https://api.github.com/users/jordan-hanson")
+        .then((response) => {
+          console.log(response.data)
+          this.setState({
+            githubUsers: response.data.name
+          }
+          )
+          console.log(this.state)
+        })
+        .catch(err => console.log(err))
+    }, 3000)
+
   };
+  handleChange = (e) => {
+    this.setState({ githubUserName: e.target.value })
+  }
+
+  handleSearch = (e) => {
+    e.preventDefault();
+    axios.get(`https://api.github.com/users/${this.state.githubUserName}`)
+      .then(resp => {
+        this.setState({
+          githubUsers: resp.data.name
+        })
+      })
+      .catch(err => console.log(err))
+  }
+
+  // componentDidUpdate(prevProps, prevState) {
+  //   console.log("App: Updated")
+  //   console.log("PROPS.........")
+  //   console.log(prevProps, this.props)
+  //   console.log("STATE..........")
+  //   console.log(prevState, this.state)
+  // };
+
   render() {
+    console.log("App: Rendered")
     return (
       <div>
         <div>
-          <h1>React GitHub User Card {this.state.name}</h1>
+          <h1>React GitHub User Card</h1>
         </div>
         <div>
-          <input placeholder="Search GitHub User"></input>
-          <button>Search</button>
+          <form onSubmit={this.handleSearch}>
+            <input type="text" onChange={this.handleChange} placeholder="Search GitHub User"></input>
+            <button >Search</button>
+          </form>
+        </div>
+        <div className="githubUserCard">
+          {/* {
+            this.state.githubUser.map(user => {
+              return (<p src={user} />)
+            })
+          } */}
+          <p>{this.state.githubUsers}</p>
         </div>
       </div>
     )
   }
 }
+
 
 export default App;
